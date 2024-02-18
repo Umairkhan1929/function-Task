65b17fa231373d8e764ffd5e4c6123ede30c1061
Umairkhan1929 <145902912+Umairkhan1929@users.noreply.github.com>
2024-02-18 01:33:19 -0800

    Add files via upload

---
+++ function-Task.js
@@ -0,0 +1,39 @@
+// Task 1 ***Welcome Message:***
+
+
+// function displayWelcomeMessage(){
+//     let userName = prompt("who is this please");
+
+//     alert("Welcome To you Mr." + userName)
+
+// }
+
+
+// displayWelcomeMessage();
+
+
+//  Task 2 ***Quote Generator:***
+
+// function generateQuote(){
+//     let array =["Just one small positive thought in the morning can change your whole day.","Opportunities don't happen, you create them.","Love your family, work super hard, live your passion.","It is never too late to be what you might have been."]
+//    let randomNumber = Math.floor(Math.random() * array.length);
+//    console.log(array[randomNumber]);
+   
+//    }
+   
+//    generateQuote()
+
+
+//  Task 3 ***Random Number Generator:***
+
+//    function generateRandomNumber(){
+
+    
+//     let randomNumber = Math.ceil(Math.random() * 100);
+//     alert(randomNumber);
+    
+    
+//     }
+    
+//     generateRandomNumber();
+    
